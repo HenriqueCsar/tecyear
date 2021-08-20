3e0b80463dd56454f49de555d74dbee785caa543
HenriqueCsar <henrique.fibonacci@hotmail.com>
2021-08-19 23:18:23 -0300

    iniciado finances

--- gui/static/js/script.js
+++ gui/static/js/script.js
@@ -1,8 +1,63 @@
-$('#sistema').click(function(){
-    hide()
-})
+setTimeout(function() { 
+        $('#sistema_menu').show()
+        $('.loading').hide()
+        $('.finances').show()
+    }, 2000
+);
 
 
-function hide(){
-    $("#sistema_menu").attr('id', 'sistema_hide');
+
+
+function devendo(){
+    $('.clientes_devendo').hide()
+    $('.pago').hide()
+    $('.devendo').show()
+    $('.vip').hide()
+    $('.gasto').hide()
+    $('.investimentos').hide()
+}
+
+function pago(){
+    $('.clientes_devendo').hide()
+    $('.pago').show()
+    $('.devendo').hide()
+    $('.vip').hide()
+    $('.gasto').hide()
+    $('.investimentos').hide()
+}
+
+function vip(){
+    $('.clientes_devendo').hide()
+    $('.pago').hide()
+    $('.devendo').hide()
+    $('.gasto').hide()
+    $('.investimentos').hide()
+    $('.vip').show()
+}
+
+function gastos(){
+    $('.clientes_devendo').hide()
+    $('.pago').hide()
+    $('.devendo').hide()
+    $('.vip').hide()
+    $('.gasto').show()
+    $('.investimentos').hide()
+}
+
+function investimentos(){
+    $('.clientes_devendo').hide()
+    $('.pago').hide()
+    $('.devendo').hide()
+    $('.vip').hide()
+    $('.gasto').hide()
+    $('.investimentos').show()
+}
+
+function clientes_devendo(){
+    $('.pago').hide()
+    $('.devendo').hide()
+    $('.vip').hide()
+    $('.gasto').hide()
+    $('.investimentos').hide()
+    $('.clientes_devendo').show()
 }
